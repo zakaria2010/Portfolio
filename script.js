2f31c91ee4115d2cbc3d9f09ce9378f743276981
zakaria-samlali <samlali.zakaria@gmail.com>
2024-04-01 23:14:46 +0000

    add html/css

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-window.alert("Hello, world!");
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+  
